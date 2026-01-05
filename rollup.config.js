aefff0a82bb4613b64cf48af3c5663287622525f
Sully <vulajsv@gmail.com>
2026-01-05 08:01:34 +0000

    Add missing newline in `rollup.config.js` for better readability.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
